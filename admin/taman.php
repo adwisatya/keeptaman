f76d6c18692bccdfe0c25d4a27e4e0ad7894d592
adwisatya <a.dwisaty4@yahoo.com>
2015-02-15 10:17:40 +0700

    delete taman

--- admin/taman.php
+++ admin/taman.php
@@ -9,6 +9,7 @@
 		$username_admin = $_POST['idadmin'];
 		switch($_GET['command']){
 			case 0 : $data = mysql_query("INSERT INTO taman (`id_taman`, `nama_taman`, `alamat`, `geolokasi`, `username_admin`) VALUES ('$id_taman', '$nama_taman','$alamat','$geolokasi','$username_admin');");
+					echo "alert('dadas')";
 					break;
 			case 1 : $data = mysql_query("DELETE FROM taman WHERE `id_taman` = '$id_taman'");
 					break;
@@ -22,7 +23,18 @@
 		$data	=	mysql_query("SELECT * from taman");
 		if(mysql_num_rows($data)>0){
 			while($user= mysql_fetch_array($data)){
-				echo $user['nama_taman'];
+?>
+		<td></td>
+		<td><?php $user['nama_taman']; ?></td>
+		<td><?php $user['alamat']; ?></td>
+		<td><?php $user['geolokasi']; ?></td>
+		<td>
+			<input type="Submit" class="btn btn-primary btn-sm btn-primary edit" value="Edit">
+		</td>
+		<td>
+			 <input type="Submit" class="btn btn-primary btn-sm btn-primary delete" value="Delete">
+		</td>
+<?php
 			}
 		}else{
 			echo "Tidak ada data";
